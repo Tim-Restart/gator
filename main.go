c0c41a11cbc567efa00c29aea9eadd7ae1d1375c
Tim-Restart <tim_restart@proton.me>
2025-06-22 13:20:11 +1000

    Working on struct declaration

--- main.go
+++ main.go
@@ -6,6 +6,33 @@ import (
 	"log"
 )
 
+type state struct {
+	config			*Config
+}
+
+// Holds the command and arguments
+type command struct {
+	cmd			string
+	args		[]string
+}
+
+type commands struct {
+	cmds 		map[string]func(*state, command) error
+}
+
+func (c *sommands) run(s *state, cmd command) error {
+	handler, ok := c.cmds[cmd.cmd] // Something here about running...?
+	if !ok {
+		return fmt.Errorf("Unknown command: %s", cmd.cmd)
+	}
+	return handler(s, cmd)
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.cmds[name] = f
+}
+
+
 func main() {
 
 	cfg, err := config.Read()
@@ -13,11 +40,26 @@ func main() {
 		log.Print("Error reading file")
 		return
 	}
+
+	state := state{
+		config: cfg,
+	}
+
+	comds := config.commands
+	
+
+	
+
+	/*
 	err = cfg.SetUser("Tim")
 	if err != nil {
 		log.Print("Error setting username")
 		return
 	}
+		*/
+
+	
+
 	cfg, err = config.Read()
 	fmt.Println(cfg)
 	return
